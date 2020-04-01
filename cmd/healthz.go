3efd78d82bcaf7a1a9655519a405a4ae805b50f1
mozillazg <mozillazg101@gmail.com>
2020-04-01 15:54:47 +0800

    Don't call kms api when checking healthz
    Call kms api is an expensive action

--- cmd/healthz.go
+++ cmd/healthz.go
@@ -4,16 +4,14 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"github.com/spf13/cobra"
-
 	"net"
 	"time"
 
 	"github.com/golang/glog"
-
-	"github.com/AliyunContainerService/ack-kms-plugin/plugin"
-	k8spb "github.com/AliyunContainerService/ack-kms-plugin/v1beta1"
+	"github.com/spf13/cobra"
 	"google.golang.org/grpc"
+
+	k8spb "github.com/AliyunContainerService/ack-kms-plugin/v1beta1"
 )
 
 // newCmdHealth provides healthcheck method for kms server.
@@ -42,10 +40,6 @@ func newCmdHealth() *cobra.Command {
 			if err := pingRPC(ctx, c, unixSocketPath); err != nil {
 				glog.Fatalf("Exit cause unhealthy rpc connection")
 			}
-
-			if err := pingKMS(ctx, c); err != nil {
-				glog.Fatalf("Exit cause unhealthy kms service")
-			}
 		},
 	}
 	command.Flags().StringVar(&unixSocketPath, "path-to-unix-socket", "/var/run/kmsplugin/socket.sock", "Full path to Unix socket that is used for communicating with KubeAPI Server, or Linux socket namespace object - must start with @")
@@ -64,26 +58,6 @@ func pingRPC(ctx context.Context, c k8spb.KeyManagementServiceClient, unixSocket
 	}
 
 	glog.V(4).Infof("Successfully pinged gRPC via %s", unixSocketPath)
-	return nil
-}
-
-func pingKMS(ctx context.Context, c k8spb.KeyManagementServiceClient) error {
-	glog.V(4).Infof("test kms service ping...")
-
-	plainText := []byte("secret")
-
-	encryptRequest := k8spb.EncryptRequest{Version: plugin.Version, Plain: plainText}
-	encryptResponse, err := c.Encrypt(ctx, &encryptRequest)
-	if err != nil {
-		return fmt.Errorf("failed to ping KMS: %v", err)
-	}
-
-	decryptRequest := k8spb.DecryptRequest{Version: plugin.Version, Cipher: encryptResponse.Cipher}
-	_, err = c.Decrypt(context.Background(), &decryptRequest)
-	if err != nil {
-		return fmt.Errorf("failed to ping KMS: %v", err)
-	}
-
 	return nil
 }
 
